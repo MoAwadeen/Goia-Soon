64779dcf227df0416c876073f477c31d509a9d62
Mohamed Awadeen <93440998+MoAwadeen@users.noreply.github.com>
2025-08-10 17:06:13 +0300

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: 'Goia - Coming Soon to the US',
   description: 'Goia is coming soon to the US. Sign up to get notified when we launch.',
   icons: {
-    icon: 'https://drive.google.com/file/d/1jRz7HMV7vaRT9bJgdKm_PscgEUeR3zf0/view?usp=drive_link',
+    icon: 'https://drive.google.com/uc?export=view&id=1jRz7HMV7vaRT9bJgdKm_PscgEUeR3zf0',
   },
 };
 
