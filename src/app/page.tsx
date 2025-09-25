1518d3aad34eb5e08759092312ff0bb95ea195dc
Mohamed Awadeen <93440998+MoAwadeen@users.noreply.github.com>
2025-09-25 19:26:50 +0300

    bbbb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,8 @@ export default function WaitlistPage() {
       </div>
       <div className="space-y-4">
         <h1 className="animate-in fade-in zoom-in-95 duration-500 text-4xl font-bold tracking-tighter text-primary drop-shadow-lg sm:text-5xl md:text-6xl">
-          Wait for us, coming soon
+                  Your Virtual Guide in Your Pocket
+                  Wait for us, coming soon
         </h1>
       </div>
       <Image
